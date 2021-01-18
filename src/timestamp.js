cbe41e7eebdc0cca2c02aa90713bfd442f7c2b2d
Alex Louden <alex@louden.com>
2021-01-18 14:29:05 +0800

    add toMillis

--- src/timestamp.js
+++ src/timestamp.js
@@ -5,23 +5,28 @@ function Timestamp(seconds, nanoseconds) {
   this.nanoseconds = nanoseconds;
 }
 
-Timestamp.fromDate = function(date) {
+Timestamp.fromDate = function (date) {
   return Timestamp.fromMillis(date.getTime());
 };
 
-Timestamp.fromMillis = function(ms) {
+Timestamp.fromMillis = function (ms) {
   var sec = Math.floor(ms / 1000);
   var ns = (ms % 1000) * 1000 * 1000;
   return new Timestamp(sec, ns);
 };
 
-Timestamp.now = function() {
+Timestamp.now = function () {
   return Timestamp.fromDate(new Date());
 };
 
 Timestamp.prototype.toDate = function () {
   var millis = this.seconds * 1000 + this.nanoseconds / (1000 * 1000);
   return new Date(millis);
+};
+
+Timestamp.prototype.toMillis = function () {
+  var millis = this.seconds * 1000 + this.nanoseconds / (1000 * 1000);
+  return millis;
 };
 
 module.exports = Timestamp;
